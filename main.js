7ecb0a31d8c1d52787f733cb059c5f2b07472beb
Hvatov Maksim <1032204364@pfur.ru>
2023-04-29 23:16:08 +0300

    Added content

--- main.js
+++ main.js
@@ -43,30 +43,37 @@ const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
 
 // Создаем и добавляем в сцену помощники точечного света и сетку
-const lightHelper = new THREE.PointLightHelper(pointLight);
-const gridHelper = new THREE.GridHelper(200, 50);
-scene.add(lightHelper, gridHelper);
+//const lightHelper = new THREE.PointLightHelper(pointLight);
+//const gridHelper = new THREE.GridHelper(200, 50);
+//scene.add(lightHelper, gridHelper);
 
 // Создаем OrbitControls - объект для управления камерой, затем добавляем его в сцену
 const controls = new OrbitControls(camera, renderer.domElement);
 
+// Функция addStar создает и добавляет на сцену случайно расположенные звезды
 function addStar() {
+    // Создаем геометрию и материал для звезды
     const geometry = new THREE.SphereGeometry(0.25,24,24)
     const material = new THREE.MeshStandardMaterial({color: 0xffffff})
+    // Создаем экземпляр звезды на основе геометрии и материала
     const star = new THREE.Mesh(geometry, material)
-
+    // Генерируем случайные координаты для звезды
     const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100))
-
+    // Устанавливаем координаты для звезды
     star.position.set(x,y,z)
+    // Добавляем звезду на сцену
     scene.add(star)
 }
 
+// Генерируем 200 звезд на сцене
 Array(200).fill().forEach(addStar)
+
+// Создаем текстуру для фона сцены
 const spaceTexture = new THREE.TextureLoader().load('./images/space.jpg')
+// Устанавливаем фон сцены равным созданной текстуре
 scene.background = spaceTexture
 
-
-// Moon
+// Создаем объект месяца с текстурой и картой нормалей
 const moonTexture = new THREE.TextureLoader().load('./images/moon.jpg')
 const normalTexture = new THREE.TextureLoader().load('./images/normal.jpg')
 
@@ -78,7 +85,30 @@ const moon = new THREE.Mesh(
     })
 )
 
+// Устанавливаем позицию месяца
+moon.position.setX(-10)
+moon.position.setZ(30)
+
+// Добавляем месяц на сцену
 scene.add(moon)
+
+// Функция moveCamera вызывается при прокрутке страницы и перемещает камеру
+function moveCamera() {
+    // Получаем размер элемента body страницы и вычисляем на сколько нужно
+    // переместить камеру
+    const t = document.body.getBoundingClientRect().top
+    // Вращение месяца вокруг своих осей
+    moon.rotation.x += 0.075
+    moon.rotation.y += 0.075
+    moon.rotation.z += 0.075
+    // Перемещение камеры в зависимости от прокрутки страницы
+    camera.position.setZ(t*-0.01)
+    camera.position.setX(t*-0.0002)
+    camera.position.setY(t*-0.0002)
+}
+
+// Добавляем обработчик прокрутки страницы на вызов функции moveCamera
+document.body.onscroll = moveCamera
 
 // Функция для анимации объекта, в данном случае - поворот тора и обновление OrbitControls
 function animate() {

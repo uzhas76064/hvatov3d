bcad3a061269490a9fe3b596fee9cd2f5f4dc941
Hvatov Maksim <1032204364@pfur.ru>
2023-04-29 22:35:02 +0300

    Changed background and added stars

--- main.js
+++ main.js
@@ -50,6 +50,21 @@ scene.add(lightHelper, gridHelper);
 // Создаем OrbitControls - объект для управления камерой, затем добавляем его в сцену
 const controls = new OrbitControls(camera, renderer.domElement);
 
+function addStar() {
+    const geometry = new THREE.SphereGeometry(0.25,24,24)
+    const material = new THREE.MeshStandardMaterial({color: 0xffffff})
+    const star = new THREE.Mesh(geometry, material)
+
+    const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100))
+
+    star.position.set(x,y,z)
+    scene.add(star)
+}
+
+Array(200).fill().forEach(addStar)
+const spaceTexture = new THREE.TextureLoader().load('./images/space.jpg')
+scene.background = spaceTexture
+
 // Функция для анимации объекта, в данном случае - поворот тора и обновление OrbitControls
 function animate() {
     requestAnimationFrame(animate);
